34f9f5c7896a3b930ab8a52c1f99ac7abc48f12c
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2024-11-20 16:12:03 +0700

    javascript final

--- main.js
+++ main.js
@@ -1,24 +1,87 @@
-const questionWrapper = document.querySelectorAll(".question_wrapper");
 const faqLinks = document.querySelectorAll(".faq_link");
-const expandBtn = document.querySelectorAll(".expand_btn");
-const shrinkBtn = document.querySelectorAll(".shrink_btn");
-const answerWrapper = document.querySelectorAll(".faq_answer_wrapper");
+const answerWrappers = document.querySelectorAll(".faq_answer_wrapper");
+const expandBtns = document.querySelectorAll(".expand_btn");
+const shrinkBtns = document.querySelectorAll(".shrink_btn");
 
 faqLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    let answer = link.parentElement.nextElementSibling;
+    const answer = link.parentElement.nextElementSibling;
 
-    if (answer.classList.contains("active")) {
-      answerWrapper.forEach((info) => info.classList.remove("active"));
-      shrinkBtn.forEach((minus) => (minus.style.display = "block"));
-      expandBtn.forEach((plus) => (plus.style.display = "none"));
-    } else {
-      answerWrapper.forEach((info) => info.classList.remove("active"));
-      answer.classList.add("active");
-      shrinkBtn.forEach((minus) => (minus.style.display = "none"));
-      expandBtn.forEach((plus) => (plus.style.display = "block"));
+    const isActive = answer.classList.toggle("active");
+
+    if (isActive) {
+      toggleFaqLinkVisibility(answer, isActive);
       link.nextElementSibling.style.display = "none";
       link.nextElementSibling.nextElementSibling.style.display = "block";
+    } else {
+      link.nextElementSibling.style.display = "block";
+      link.nextElementSibling.nextElementSibling.style.display = "none";
     }
   });
 });
+
+expandBtns.forEach((expand) => {
+  expand.addEventListener("click", () => {
+    const answer = expand.parentElement.nextElementSibling;
+
+    const isActive = answer.classList.toggle("active");
+
+    toggleFaqLinkVisibility(answer, isActive);
+
+    if (isActive) {
+      expand.style.display = "none";
+      expand.nextElementSibling.style.display = "block";
+    } else {
+      expand.style.display = "block";
+      expand.nextElementSibling.style.display = "none";
+    }
+  });
+});
+
+shrinkBtns.forEach((shrink) => {
+  shrink.addEventListener("click", () => {
+    const answer = shrink.parentElement.nextElementSibling;
+
+    const isActive = answer.classList.toggle("active");
+
+    toggleShrinkButtonVisibility(answer, isActive);
+
+    if (isActive) {
+      shrink.previousElementSibling.style.display = "none";
+      shrink.style.display = "block";
+    } else {
+      shrink.previousElementSibling.style.display = "block";
+      shrink.style.display = "none";
+    }
+  });
+});
+
+function toggleFaqLinkVisibility(answer, isActive) {
+  shrinkBtns.forEach(
+    (shrink) => (shrink.style.display = isActive ? "none" : "block")
+  );
+  expandBtns.forEach(
+    (expand) => (expand.style.display = isActive ? "block" : "none")
+  );
+
+  answerWrappers.forEach((info) => {
+    if (info !== answer) {
+      info.classList.remove("active");
+    }
+  });
+}
+
+function toggleShrinkButtonVisibility(answer, isActive) {
+  shrinkBtns.forEach(
+    (shrink) => (shrink.style.display = isActive ? "block" : "none")
+  );
+  expandBtns.forEach(
+    (expand) => (expand.style.display = isActive ? "none" : "block")
+  );
+
+  answerWrappers.forEach((info) => {
+    if (info !== answer) {
+      info.classList.remove("active");
+    }
+  });
+}
